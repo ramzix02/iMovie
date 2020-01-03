56f20e6f3ad3e63755d8c99659bc09de68836e36
Ahmed Ramzy <ahmedramzy@Ahmeds-Mac.local>
2020-01-02 23:23:49 -0800

    Initial Commit

---
+++ iMovie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iMovie
+//
+//  Created by Ahmed Ramzy on 1/2/20.
+//  Copyright © 2020 Ahmed Ramzy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
